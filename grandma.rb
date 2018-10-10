c6b5e7287cfe88a6cab9ca60f8a040d7971050d1
Matthew Migliore <mmigliore311@gmail.com>
2018-10-10 13:06:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == ""
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts ""
   elsif phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   else
